22c9ce146147cd7d14d4dec402576669fc0d4b64
yusuf taş <yusuf.yusuf21@hotmail.com>
2021-02-08 22:26:27 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1985, FirstName = "ENGİN", LastName = "DEMİROĞ", IdentityNumber= 12345});
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
